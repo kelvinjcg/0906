f50f6094dd262f5c035b66e63e5e49bb040f440c
kelvinjcg <guerrerokj@gmail.com>
2016-09-06 19:06:19 -0500

    ubicacion Cartagena y fragment alto 600dp

--- app/src/main/java/com/j/k/a0906/MapsActivity0906.java
+++ app/src/main/java/com/j/k/a0906/MapsActivity0906.java
@@ -39,7 +39,7 @@ public class MapsActivity0906 extends FragmentActivity implements OnMapReadyCall
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(4.595295, -74.115372);
+        LatLng sydney = new LatLng(10.406927, -75.504652);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         // agregado para prueba de versión 1.1
